f9e335b0f7d096d6ebd57ffebd51a27265b68fd5
Frederik Østergaard <fkoe90@gmail.com>
2020-03-19 16:42:12 +0100

    getModTier now works for all default mods (non influencer mods)

--- utility/generateModTierList.ts
+++ utility/generateModTierList.ts
@@ -1,14 +1,15 @@
 import {
   ModOutputDict,
   BaseItemDict,
-  ModOutput
+  ModOutput,
+  BaseItem,
+  Mod
 } from "../src/Common/Crafting/interfaces";
 
 const MODLIST: ModOutputDict = require("../src/assets/poe_data/mods.min.json");
 const BASEITEMLIST: BaseItemDict = require("../src/assets/poe_data/base_items.min.json");
 
 // get list of tags that matter for the tierlist
-
 let relevantTags = new Set();
 for (let [modName, modData] of Object.entries(MODLIST)) {
   if (!modIsWithin(modData, ["item"], ["suffix", "prefix"])) {
@@ -21,9 +22,69 @@ for (let [modName, modData] of Object.entries(MODLIST)) {
   }
 }
 
-const tierList: any = {};
+interface ModDetails {
+  reqLevel: number;
+  modId: string;
+  generationType: string;
+}
+/** The actual tier lists are stored here under tier groups */
+export interface TierList {
+  [tierGroup: string]: {
+    tags: string[];
+    itemCount: number;
+    modTypes: {
+      [tierType: string]: ModDetails[];
+    };
+  };
+}
+
+const tierListExample = {
+  tierGroupName1: {
+    tags: ["tag1", "tag2", "tag3", "tag4"],
+    itemCount: 3,
+    modTypes: {
+      modType1: [
+        {
+          reqLevel: 15,
+          modName: "WeaponElementalDamageOnWeapons1",
+          generationType: "prefix"
+        },
+        {
+          reqLevel: 30,
+          modName: "WeaponElementalDamageOnWeapons2",
+          generationType: "prefix"
+        },
+        {
+          reqLevel: 60,
+          modName: "WeaponElementalDamageOnWeapons3",
+          generationType: "prefix"
+        }
+      ],
+      modType2: [
+        {
+          reqLevel: 15,
+          modName: "funMod1",
+          generationType: "suffix"
+        },
+        {
+          reqLevel: 31,
+          modName: "funMod2",
+          generationType: "suffix"
+        },
+        {
+          reqLevel: 64,
+          modName: "funMod3",
+          generationType: "suffix"
+        }
+      ]
+    }
+  }
+};
+
+const tierList: TierList = {};
 /** mapping used for getting all tierGroups that a specific tag is used in */
-const tagToTierGroupLookup = {};
+const tagToTierGroupLookup: { [tag: string]: string[] } = {};
+// Generate tier groups based on available base items
 for (let [itemName, itemData] of Object.entries(BASEITEMLIST)) {
   if (itemData.domain !== "item") {
     continue;
@@ -32,7 +93,11 @@ for (let [itemName, itemData] of Object.entries(BASEITEMLIST)) {
   const tierGroupTags = itemData.tags.filter(e => relevantTags.has(e));
   const tierGroupName = tierGroupTags.join("#");
   if (!tierList[tierGroupName]) {
-    tierList[tierGroupName] = { tags: tierGroupTags, itemCount: 1 };
+    tierList[tierGroupName] = {
+      tags: tierGroupTags,
+      itemCount: 1,
+      modTypes: {}
+    };
     tierGroupTags.forEach(tag => {
       tagToTierGroupLookup[tag]
         ? tagToTierGroupLookup[tag].push(tierGroupName)
@@ -42,10 +107,15 @@ for (let [itemName, itemData] of Object.entries(BASEITEMLIST)) {
     tierList[tierGroupName]["itemCount"] += 1;
   }
 }
-// console.log(tagToTierLookup);
 
-// generate tierlist
-for (let [modName, modData] of Object.entries(MODLIST)) {
+/** Get the tier group a specific baseitem belongs to */
+const getTierGroup = (baseItem: Pick<BaseItem, "tags">): string => {
+  const tierGroupTags = baseItem.tags.filter(e => relevantTags.has(e));
+  return tierGroupTags.join("#");
+};
+
+// generate tierlist, inserting mods into tier groups generated above
+for (let [modId, modData] of Object.entries(MODLIST)) {
   if (!modIsWithin(modData, ["item"], ["suffix", "prefix"])) {
     continue;
   }
@@ -55,23 +125,23 @@ for (let [modName, modData] of Object.entries(MODLIST)) {
       // add the mod to all tierlists that it should be added to
 
       for (const tierGroup of tagToTierGroupLookup[spawnWeight.tag]) {
-        // avoid dublicate mods in tierlists
+        // avoid duplicate mods in tierlists
         if (AddedToTierGroups[tierGroup]) {
           continue;
         }
         AddedToTierGroups[tierGroup] = true;
-        if (!tierList[tierGroup][modData.type]) {
-          tierList[tierGroup][modData.type] = [
+        if (!tierList[tierGroup].modTypes[modData.type]) {
+          tierList[tierGroup].modTypes[modData.type] = [
             {
               reqLevel: modData.required_level,
-              modName,
+              modId,
               generationType: modData.generation_type
             }
           ];
         } else {
-          tierList[tierGroup][modData.type].push({
+          tierList[tierGroup].modTypes[modData.type].push({
             reqLevel: modData.required_level,
-            modName,
+            modId,
             generationType: modData.generation_type
           });
         }
@@ -80,8 +150,47 @@ for (let [modName, modData] of Object.entries(MODLIST)) {
   }
 }
 
-console.log(tierList["rapier#sword#one_hand_weapon#weapon#default"]);
-//console.log(Object.keys(tierList));
+// Go through all tier lists and sort them on requiredLevel
+for (const [tierGroupName, tierGroup] of Object.entries(tierList)) {
+  for (const [tierTypeName, tierType] of Object.entries(tierGroup.modTypes)) {
+    tierType.sort((a, b) => b.reqLevel - a.reqLevel);
+  }
+}
+
+const getModTier = (
+  baseItem: Pick<BaseItem, "tags">,
+  mod: Pick<Mod, "type" | "key">
+): undefined | number => {
+  const tierGroup = getTierGroup(baseItem);
+  const tierListEntry = tierList[tierGroup]?.modTypes[mod.type];
+  if (!tierListEntry) {
+    return undefined;
+  }
+
+  const tier = tierListEntry.findIndex(e => e.modId === mod.key);
+  return tier === -1 ? undefined : tier + 1;
+};
+
+console.log(tierList["dex_int_armour#boots#armour#default"].modTypes);
+
+const testBaseItem = {
+  tags: [
+    "not_for_sale",
+    "atlas_base_type",
+    "bootsatlas1",
+    "dex_int_armour",
+    "boots",
+    "armour",
+    "default"
+  ]
+};
+
+const testMod = {
+  type: "LifeRegeneration",
+  key: "LifeRegeneration1"
+};
+
+console.log(getModTier(testBaseItem, testMod));
 
 /*
     Utility functions
